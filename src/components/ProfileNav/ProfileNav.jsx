8f500e322e8a59193aa0b3a1edbc71425aaa77e2
Thegodlessman <luischang16@hotmail.com>
2024-09-23 14:05:20 -0400

    Nota: se ha cambiado el sidebar del profile

--- src/components/ProfileNav/ProfileNav.jsx
+++ src/components/ProfileNav/ProfileNav.jsx
@@ -1,10 +1,15 @@
 import React, {useState} from 'react';
+import {Layout, Menu} from 'antd';
+import {HomeOutlined, SettingOutlined} from '@ant-design/icons';
 import jwt_decode from 'jwt-decode'
 
 import './ProfileNav.css'
 
+const {Sider} = Layout;
+
 function ProfileNav(){
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(true);
+    const [isLogout, setIsLoguot] = useState(false);
 
     let token = localStorage.getItem('token');
     let decodedToken = jwt_decode(token);
@@ -23,32 +28,35 @@ function ProfileNav(){
 
     return (
         <>
-            <div className={`sidebar-container_profile ${isOpen ? 'open' : 'closed'}`}>
-                <div className="sidebar_profile" onClick={toggleSidebar}>
-                    {isOpen ? (  // Solo mostrar contenido cuando esté abierto
-                        <div className="sidebar-header_profile" >
+            <Layout>
+                <Sider className='sidebar-container_profile'>
+                    <div className='fs-3 fw-bolder text-decoration-none me-5 ms-4 sidebar-logo'  href="/">
+                        <img className="me-1" src='../../../src/assets/logo.png' width="50" height="50"/>
+                        Educativa
+                    </div>
+                    <Menu theme='dark' mode='inline' className='sidebar-menu_profile'>
+                    
+                        <button><HomeOutlined/>Home</button>
+
+                        
+                    </Menu>
+
+                    <div className="sidebar-account_profile" >
                             <div className="sidebar-img_profile">
                                 <img src='https://thumbs.dreamstime.com/z/s%C3%ADmbolo-de-perfil-masculino-inteligente-retrato-estilo-caricatura-m%C3%ADnimo-166146967.jpg' alt='Imagen de perfil'></img>
                             </div>
 
                             <div className="sidebar-details_profile">
-                                <p className="sidebar-username_profile"> {user_fullname}</p>
+                                <p className="sidebar-username_profile"> {user_fullname}
                                 <p className="sidebar-rol_profile">{rol}</p>
+                                </p>
                             </div>
-                        </div>
-                    ) : (
-                        /* HTML cuando el sidebar está cerrado */
-                            <div className="sidebar-header_profile">
-                                <div className="sidebar-img_profile">
-                                    <img 
-                                        src='https://thumbs.dreamstime.com/z/s%C3%ADmbolo-de-perfil-masculino-inteligente-retrato-estilo-caricatura-m%C3%ADnimo-166146967.jpg' 
-                                        alt='Imagen de perfil'
-                                    />
-                                </div>
-                            </div>
-                    )}
-                </div>
-            </div>
+
+                            
+                    </div>
+                </Sider>
+
+            </Layout>
         </>
     )
 }
