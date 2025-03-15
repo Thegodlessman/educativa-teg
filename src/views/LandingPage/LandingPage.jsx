b933d55c7274970e98705e4c8f258033d7772ecc
Thegodlessman <luischang16@hotmail.com>
2025-03-15 09:47:50 -0400

    Feat: Se ha creado el landingpage

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -1,10 +1,20 @@
 import NavBar from "../../components/Navbar/Navbar";
+import './LandingPage.css'
+import welImg from '../../assets/Welcome.png'
 
 function LandingPage(){
     return(
         <>
             <NavBar/>
-            <h1>Estas en la vista inicial</h1>
+            <div className="landing-container">
+                <div className="landing-container-welcome">
+                    <div className="landing-container-welcome-text">
+                            <h1 className="welcome-title">¡Bienvenido a Educativa! </h1>
+                            <span className="welcome-text">Una plataforma diseñada para mejorar el aprendizaje y ofrecer herramientas de apoyo a toda la comunidad educativa. </span>
+                    </div>
+                    <img className="welcome-img" src={welImg}alt="Imagen de bienvenida" />
+                </div>
+            </div>
         </>
     )
 }
