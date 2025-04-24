0875e4fc70fdf5ceea439723120ca21c066d6222
Thegodlessman <luischang16@hotmail.com>
2025-04-23 20:53:41 -0400

    Feat: Se ha empezado con el uso de Cloudnary para alojar las imagenes

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -5,13 +5,14 @@ import './LandingPage.css'
 import axios from 'axios';
 
 import SelectRole from '../../components/SelectRole/SelectRole.jsx';
-import welImg from '../../assets/Welcome.png'
 import { notifyError, notifySuccess } from '../../utils/notify.js';
 
 function LandingPage(){
     const [activeRole, setActiveRole] = useState(null);
     const [showModal, setShowModal] = useState(false); // Estado que controla la visibilidad del modal
     const [userId, setUserId] = useState('');
+
+    const welImg = import.meta.env.VITE_CLOUDNARY_IMAGE + 'educativa/Welcome_aa2b03'
 
     useEffect(() => {
         const token = localStorage.getItem('token');
