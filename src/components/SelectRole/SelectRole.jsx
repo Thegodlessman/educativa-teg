9e5bc661bb9afdf5610db162008f706000b87509
Thegodlessman <luischang16@hotmail.com>
2025-04-26 13:15:23 -0400

    FEAT: Mejoras y estilos a los inputs

--- src/components/SelectRole/SelectRole.jsx
+++ src/components/SelectRole/SelectRole.jsx
@@ -17,24 +17,101 @@ function SelectRole({ show, handleClose, handleRoleChange }) {
   const [roles, setRoles] = useState([]);
   const [selectedRole, setSelectedRole] = useState(null);
   const [selectedInstitution, setSelectedInstitution] = useState(null);
-  const [photo, setPhoto] = useState(null);
   const [selectedImage, setSelectedImage] = useState(null);
   const [previewUrl, setPreviewUrl] = useState(null);
   const [userData, setUserData] = useState(null);
   const [institutions, setInstitutions] = useState([]);
 
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [state, setState] = useState([]);
+  const [selectedState, setSelectedState] = useState(null)
+  const [muni, setMuni] = useState([]);
+  const [selectedMuni, setSelectedMuni] = useState(null)
+  const [parish, setParish] = useState([]);
+  const [selectedParish, setSelectedParish] = useState(null)
+
   useEffect(() => {
     if (show) {
       fetchRoles();
-      fetchInstitutions();
+      fetchCountries();
+
       setStep(1);
     }
   }, [show]);
 
+  useEffect(() => {
+    if (selectedCountry) {
+      fetchStates();
+    }
+  }, [selectedCountry]);
+
+  useEffect(() => {
+    if (selectedState) {
+      fetchMunicipalities();
+    }
+  }, [selectedState]);
+
+  useEffect(() => {
+    if (selectedMuni) {
+      fetchParishes();
+    }
+  }, [selectedMuni]);
+
+  useEffect(()=>{
+    if(selectedParish){
+      fetchInstitutions()
+    }
+  },[selectedParish])
+
+  const fetchCountries = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:4555/profile/get/countries"
+      );
+      setCountries(response.data.countries);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchStates = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:4555/profile/get/stateByCountries/${selectedCountry}`
+      );
+      setState(response.data.states);
+    } catch (error) {
+      console.error("Error al cargar los estados:", error);
+    }
+  };
+
+  const fetchMunicipalities = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:4555/profile/get/munByStates/${selectedState}`
+      );
+      setMuni(response.data.mun);
+    } catch (error) {
+      console.error("Error al cargar municipios:", error);
+    }
+  };
+
+  const fetchParishes = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:4555/profile/get/parishesByMunicipality/${selectedMuni}`
+      );
+      setParish(response.data.parishes);
+    } catch (error) {
+      console.error("Error al cargar parroquias:", error);
+    }
+  };
+
   const fetchInstitutions = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4555/profile/get/institutions"
+        `http://localhost:4555/profile/get/institutionsByParish/${selectedParish}`
       );
       setInstitutions(response.data.institutions);
     } catch (error) {
@@ -103,6 +180,47 @@ function SelectRole({ show, handleClose, handleRoleChange }) {
     }
   };
 
+  const handleCountryChange = (e) => {
+    const value = e.target.value;
+    setSelectedCountry(value);
+    setSelectedState(null);
+    setSelectedMuni(null);
+    setSelectedParish(null);
+    setSelectedInstitution(null);
+    setState([]);
+    setMuni([]);
+    setParish([]);
+    setInstitutions([]);
+  };
+  
+  const handleStateChange = (e) => {
+    const value = e.target.value;
+    setSelectedState(value);
+    setSelectedMuni(null);
+    setSelectedParish(null);
+    setSelectedInstitution(null);
+    setMuni([]);
+    setParish([]);
+    setInstitutions([]);
+  };
+  
+  const handleMunicipalityChange = (e) => {
+    const value = e.target.value;
+    setSelectedMuni(value);
+    setSelectedParish(null);
+    setSelectedInstitution(null);
+    setParish([]);
+    setInstitutions([]);
+  };
+  
+  const handleParishChange = (e) => {
+    const value = e.target.value;
+    setSelectedParish(value);
+    setSelectedInstitution(null);
+    setInstitutions([]);
+  };
+  
+
   return (
     <Modal
       show={show}
@@ -157,16 +275,79 @@ function SelectRole({ show, handleClose, handleRoleChange }) {
 
             {step === 2 && (
               <>
-                <h5>Selecciona tu institución</h5>
-                <div className="form-group mt-4">
+              <h5>Ubicación de la institución</h5>
+              <div className="select-group mt-4">
+                <div className="form-group">
+                  <select
+                    className="form-select"
+                    value={selectedCountry || ""}
+                    onChange={(e) => handleCountryChange(e)}
+                  >
+                    <option value="" disabled>-- Selecciona un país</option>
+                    {countries.map((c) => (
+                      <option key={c.id_country} value={c.id_country}>
+                        {c.country_name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+            
+                <div className="form-group">
+                  <select
+                    className="form-select"
+                    value={selectedState || ""}
+                    onChange={(e) => handleStateChange(e)}
+                    disabled={!selectedCountry}
+                  >
+                    <option value="" disabled>-- Selecciona un estado </option>
+                    {state.map((st) => (
+                      <option key={st.id_state} value={st.id_state}>
+                        {st.state_name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+            
+                <div className="form-group">
+                  <select
+                    className="form-select"
+                    value={selectedMuni || ""}
+                    onChange={(e) => handleMunicipalityChange(e)}
+                    disabled={!selectedState}
+                  >
+                    <option value="" disabled>-- Selecciona un municipio</option>
+                    {muni.map((m) => (
+                      <option key={m.id_municipality} value={m.id_municipality}>
+                        {m.municipality_name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+            
+                <div className="form-group">
+                  <select
+                    className="form-select"
+                    value={selectedParish || ""}
+                    onChange={(e) => handleParishChange(e)}
+                    disabled={!selectedMuni}
+                  >
+                    <option value="" disabled>-- Selecciona una parroquia</option>
+                    {parish.map((p) => (
+                      <option key={p.id_parish} value={p.id_parish}>
+                        {p.parish_name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+            
+                <div className="form-group" style={{ flex: "1 1 100%" }}>
                   <select
                     className="form-select"
                     value={selectedInstitution || ""}
                     onChange={(e) => setSelectedInstitution(e.target.value)}
+                    disabled={!selectedParish}
                   >
-                    <option value="" disabled>
-                      -- Selecciona una institución --
-                    </option>
+                    <option value="" disabled>-- Selecciona una institución</option>
                     {institutions.map((inst) => (
                       <option key={inst.id_insti} value={inst.id_insti}>
                         {inst.insti_name}
@@ -174,7 +355,9 @@ function SelectRole({ show, handleClose, handleRoleChange }) {
                     ))}
                   </select>
                 </div>
-              </>
+              </div>
+            </>
+            
             )}
 
             {step === 3 && (
